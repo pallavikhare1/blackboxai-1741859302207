d3be20ac2ef702387addd74d1a64cc7bfe7296f6
pallavikhare1 <147231354+pallavikhare1@users.noreply.github.com>
2025-03-13 15:27:07 +0530

    Add blank-app/src/App.tsx 1741859827153

---
+++ blank-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { DemoBackgroundPaths } from './components/demo';
+
+function App() {
+  return (
+    <>
+      <div><DemoBackgroundPaths/></div>
+    </>
+  );
+}
+
+export default App;
